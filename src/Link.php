69f863919bcbbbaea736a035b535922a22a02fc9
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2019-09-30 16:37:54 +0200

    Set meta at resolve time
    This allows to provide some defaults in order to avoid users
    to have to specify a text and newTab details as they are often
    implemented in the same way.

--- src/Link.php
+++ src/Link.php
@@ -13,12 +13,35 @@ class Link extends Field
      */
     public $component = 'link';
 
-    public function details($details)
+    /**
+     * The field's details given by the user.
+     *
+     * @var array
+     */
+    private $details;
+
+    public function resolve($resource, $attribute = null)
     {
-        return $this->withMeta([
+        parent::resolve($resource, $attribute);
+
+        $details = array_merge([
+            'text' => function () {
+                return $this->value;
+            },
+            'newTab' => false,
+        ], $this->details);
+
+        $this->withMeta([
             'href' => is_callable($details['href']) ? call_user_func($details['href']) : $details['href'],
             'text' => is_callable($details['text']) ? call_user_func($details['text']) : $details['text'],
             'newTab' => is_callable($details['newTab']) ? call_user_func($details['newTab']) : $details['newTab'],
         ]);
+    }
+
+    public function details($details)
+    {
+        $this->details = $details;
+
+        return $this;
     }
 }
